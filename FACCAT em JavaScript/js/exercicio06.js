cea10ce161f5bf0d1404dc83d0effc91b5be2625
Amanda Valentim <101263543+amandavalentim@users.noreply.github.com>
2022-06-16 18:14:45 -0300

    Add files via upload

---
+++ FACCAT em JavaScript/js/exercicio06.js
@@ -0,0 +1,7 @@
+let base, altura, area
+
+base = parseInt(prompt("Informe o valor da base do retângulo: "));
+altura = parseInt(prompt("Agora, informe o valor da altura do retângulo: "));
+area = base*altura
+
+alert("A área do retângulo será: "+area)
