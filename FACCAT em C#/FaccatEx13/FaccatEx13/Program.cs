6c600416c7e94d168f2fe8a285ee3cd2681fd23b
Amanda Valentim <101263543+amandavalentim@users.noreply.github.com>
2024-02-01 10:25:31 -0300

    Add files via upload

---
+++ FACCAT em C#/FaccatEx13/FaccatEx13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Digite a primeira nota do aluno: ");
+int nota1 = int.Parse(Console.ReadLine());
+
+Console.Write("Digite a segunda nota do aluno: ");
+int nota2 = int.Parse(Console.ReadLine());
+
+Console.Write("Digite a terceira nota do aluno: ");
+int nota3 = int.Parse(Console.ReadLine());
+
+int mediaFinal = nota1 * 2 + nota2 * 3 + nota3 * 5 / 10;
+
+Console.Write($"A média final do aluno é: {mediaFinal}");
