21b140dc05fd3c401c056e45ccf0b03a7337ddad
Amanda Valentim <101263543+amandavalentim@users.noreply.github.com>
2024-02-01 10:22:17 -0300

    Add files via upload

---
+++ FACCAT em C#/FaccatEx11/FaccatEx11/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write(" Carros vendidos: ");
+float carros = float.Parse(Console.ReadLine());
+
+Console.Write(" Valor total das vendas: ");
+float vendas = float.Parse(Console.ReadLine());
+
+Console.Write(" Salário fixo: ");
+float salario = float.Parse(Console.ReadLine());
+
+Console.Write(" Valor recebido por carro vendido: ");
+float comissão = float.Parse(Console.ReadLine());
+
+double porcentagem = (5 / 100) * carros;
+
+double sFinal = porcentagem + vendas + salario + comissão;
+
+Console.WriteLine($"Salário final do vendedor: {sFinal}");
