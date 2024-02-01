4e844d7f102d214986ac26947483f5118dc5fb58
Amanda Valentim <101263543+amandavalentim@users.noreply.github.com>
2024-02-01 10:14:22 -0300

    Add files via upload

---
+++ FACCAT em C#/FaccatEx6/FaccatEx6/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Digite a base de um retângulo: ");
+
+        int baseR = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a altura de um retângulo: ");
+        int altura = int.Parse(Console.ReadLine());
+
+        double area = baseR * altura;
+
+        Console.WriteLine($"a área do retângulo é: {area}");
+
+    }
+}
