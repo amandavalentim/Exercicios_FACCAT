864adde42d31f6af3c1a80bbbfd8856ee427176e
Amanda Valentim <101263543+amandavalentim@users.noreply.github.com>
2024-02-01 10:20:31 -0300

    Add files via upload

---
+++ FACCAT em C#/FaccatEx10/FaccatEx10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Qual é o custo de fábrica do carro?");
+double cFabrica = double.Parse(Console.ReadLine());
+
+double distribuidor = (28 / 100) * cFabrica;
+double impostos = (45 / 100) * cFabrica;
+
+double cFinal = cFabrica + distribuidor + impostos;
+
+Console.WriteLine($"Custo final ao consumidor é: {cFinal}");
