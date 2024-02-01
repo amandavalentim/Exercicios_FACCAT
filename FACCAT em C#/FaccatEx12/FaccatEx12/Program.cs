ad5cfcdec01c3f390ffebc570922a908d167006f
Amanda Valentim <101263543+amandavalentim@users.noreply.github.com>
2024-02-01 10:23:10 -0300

    Add files via upload

---
+++ FACCAT em C#/FaccatEx12/FaccatEx12/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Temperatura em Fahrenheit: ");
+double F = double.Parse(Console.ReadLine());
+
+double C = 5 / 9 * (F - 32);
+
+Console.Write($" Temperatura em graus celsius: {C}");
+
